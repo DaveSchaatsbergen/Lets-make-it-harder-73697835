b3f4d2e22074ba7c31ff621b83e43eb75413fff6
Dave Schaatsbergen <45204337+DaveSchaatsbergen@users.noreply.github.com>
2020-05-07 14:23:46 +0200

    Add files via upload

--- scale.php
+++ scale.php
@@ -1,10 +1,34 @@
-<?php
-$links = 5;
-$rechts = 5;
-if($links == $rechts) {
-    echo "In belans" . "<br>";
-    echo "Waardes zijn: Links = " .$links. " rechts = " .$rechts;
-} else {
-    echo "Niet in belans";
-    echo "Waardes zijn: Links = " .$links. " rechts = " .$rechts;
-}
+<?php 
+function arrayCheck($Array, $arr_size, $sum) 
+{ 
+    $l;
+    $r; 
+  
+    /* Sorteer de array */
+    sort($Array); 
+  
+    /*check voor de 2 waardes in de array*/
+    $l = 0; 
+    $r = $arr_size - 1;  
+    while ($l < $r) 
+    { 
+        if($Array[$l] + $Array[$r] == $sum) 
+            return 1;  
+        else if($Array[$l] + $Array[$r] < $sum) 
+            $l++; 
+        else 
+            $r--; 
+    }  
+    return 0; 
+} 
+  
+$Array = array (1, 4, 45, 12, 16, -8); 
+$n = 16; 
+$arr_size = sizeof($Array); 
+  
+// roep de functie op
+if(arrayCheck($Array, $arr_size, $n)) 
+    echo "In balans"; 
+else
+    echo "Niet in balans"; 
+?> 
